a72032868dbc39daa6020f35272207992a676790
Dan Bikle <dan@bot4.us>
2011-09-10 18:56:05 -0400

    c1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitt::Application.config.secret_token = '44fc83d57fc069bab456896fd4a0ad844717aa9bb0a8884fb3b3f25a3fa93cee0adb32c097fe0b2a9032ce0bde15c466f5d66f1e1646304a3a2b34504d31d513'
